67d6082f40197f27a2777ee78817c2899837d15b
Alvin Tan De Jun <>
2020-09-11 17:26:52 +0800

    Add example of using generics using Cage class

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -3,6 +3,7 @@ package circus;
 import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
+import circus.animal.Tiger;
 import circus.stuff.Cage;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
@@ -61,9 +62,14 @@ public class Circus {
         Cage<Parrot> parrotCage = new Cage<>();
         parrotCage.lockUp(parrot);
 
+        Tiger tiger = new Tiger("Sherkhan");
+        Cage<Tiger> tigerCage = new Cage<>();
+        tigerCage.lockUp(tiger);
+
         ArrayList<Cage> cages = new ArrayList<>();
         cages.add(duckCage);
         cages.add(parrotCage);
+        cages.add(tigerCage);
 
         for(Cage c: cages) {
             c.release();
